1521f340d64c238cd7af4bf934c32c1756b3247d
4rkh4m <4rkham@proton.me>
2024-07-19 23:47:22 +0200

    chore(fivem): setup fivem specific structure

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author '4rkh4m'
+description 'Resource template for FiveM'
+version '0.1.0'
+
+lua54 'yes'
+
+client_scripts { 'client/**/*.lua' }
+server_scripts { 'server/**/*.lua' }
+shared_scripts { 'shared/**/*.lua' }
